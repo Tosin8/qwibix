d6aedb57279731a93c714eca2e49e90acb294661
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 16:12:01 +0100

    Create image_strings.dart

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,8 @@
+class BImages {
+  static const String darkApplogo = '';
+  static const String lightApplogo = '';
+
+  // social media
+  static const String facebook = '';
+  static const String google = '';
+}
