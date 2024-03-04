2ada6a8fa39cbf126d4645721d37b510999b95c0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 13:00:45 +0100

    Refactoring with app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'screen/splash/splash_screen.dart';
-import 'utils/theme/theme.dart';
+import 'app.dart';
+
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Belly Max',
-      themeMode: ThemeMode.system,
-      // theme: ThemeData(
-        
-      
-      //   colorScheme: ColorScheme.fromSeed(
-      //     seedColor: Colors.deepPurple),
-      //   useMaterial3: true,
-      // ),
-
-      theme: BAppTheme.lightTheme, 
-      darkTheme: BAppTheme.darkTheme,
-      home: SplashScreen()
-    ); 
-    
-  }
 }
 
