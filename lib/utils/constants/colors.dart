919014ad875433bb29cb64acda36b766dc91fba8
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 13:20:17 +0100

    Update colors.dart

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -20,7 +20,17 @@ class BColors{
 
   // background container Colors
 
-  static const Color containerPrimary = Color(0xFFb0c7ff);
+  static const Color lightContainer = Color(0xFFf6f6f6);
+  static Color darkContainer = BColors.white.withOpacity(0.1); 
+
+  // button Colors
+  static const Color buttonPrimary = Color(0xff4b68ff); 
+  static const Color buttonSecondary = Color(0xff6c757d); 
+  static const Color buttonDisabled = Color(0xffc4c4c4); 
+
+  // border Colors
+  static const Color borderPrimary = Color(0xffd9d9d9); 
+  static const Color borderSecondry = Color(0xdde6e6e6); 
   
 
 }
