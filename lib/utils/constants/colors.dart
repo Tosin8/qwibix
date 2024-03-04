79cd8cfac622ecc1305651226c4052ff5bf0fa84
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 13:11:36 +0100

    Create colors.dart

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BColors{
+  BColors._(); 
+
+// Basic Color
+  static const Color primary = Color(0xFF4b68FF); 
+  static const Color secondary = Color(0xFFFFe24b); 
+  static const Color primaryColor = Color(0xFFb0c7ff); 
+
+  // text Color
+  static const Color textPrimary = Color(0xFF333333); 
+  static const Color textSecondary = Color(0xFF6c757d); 
+  static const Color textWhite = Colors.white; 
+
+  // background Colors
+  static const Color light = Color(0xfff6f6f6); 
+  static const Color dark = Color(0xff272727);
+  static const Color primaryBackground = Color(0xfff3f5ff);  
+
+  // background container Colors
+  
+  
+  
+
+}
