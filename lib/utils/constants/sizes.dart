e33ab8e9169f5e8b419f61c5a756d45b00bde42f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 15:41:13 +0100

    Update sizes.dart

--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -1,4 +1,37 @@
 class BSizes {
   // Padding and Margin Sizes
-  
+  static const double xs = 4.0; 
+  static const double sm = 8.0; 
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  // icon sizes
+  static const double iconXs = 12.0; 
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+  // font sizes
+  static const double fontSizesm = 14.0; 
+  static const double fontSizemd = 16.0;
+  static const double fontSizelg = 18.0;
+
+  // button sizes
+  static const double buttonHeight = 18.0; 
+  static const double buttonWidth = 120.0;
+  static const double buttonRadius = 12.0; 
+  static const double buttonElevation = 4.0; 
+
+  // AppBar height
+  static const double appBarHeight = 56.0; 
+
+  // Image sizes
+  static const double imageThumbSize = 80.0;
+
+  // default spacing between sections
+  static const double defaultSpace = 24.0; 
+  static const double spaceBtwItems = 16.0; 
+  static const double spaceBtwSections = 32.0; 
+
 }
