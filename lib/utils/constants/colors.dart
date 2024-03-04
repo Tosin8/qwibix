c9082b0c30e77b51bc78d0945327722623c965de
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 14:49:35 +0100

    Update colors.dart

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -33,5 +33,21 @@ class BColors{
   static const Color borderPrimary = Color(0xffd9d9d9); 
   static const Color borderSecondry = Color(0xdde6e6e6); 
   
+  // error and validation Colors 
+  static const Color error = Color(0xffd32f2f);
+  static const Color success = Color(0xff388e3c); 
+  static const Color warning = Color(0xfff57c00);
+  static const Color info = Color(0xff1976d2); 
+
+  // neutral shades
+  static const Color black = Color(0xff232323); 
+  static const Color darkerGrey = Color(0xff4f4f4f);
+  static  const Color darkGrey = Color(0xff939393);
+  static const Color grey = Color(0xffe0e0e0);
+  static const Color softGrey = Color(0xfff4f4f4);
+  static const Color lightGrey = Color(0xfff9f9f9);
+  static const Color white = Color(0xfffffff); 
+
+
 
 }
