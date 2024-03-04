5c02a31e76e10443a0f2b435394f3949a9685491
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 10:18:21 +0100

    Create outline button theme

--- lib/utils/theme/custom_themes/chip_theme.dart
+++ lib/utils/theme/custom_themes/chip_theme.dart
@@ -10,4 +10,12 @@ class BChipTheme {
     padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
     checkmarkColor: Colors.white, 
   ); 
+
+  static ChipThemeData darkChipTheme = ChipThemeData(
+    disabledColor: Colors.grey.withOpacity(0.4), 
+    labelStyle: const TextStyle(color: Colors.white), 
+    selectedColor: Colors.blue, 
+    padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+    checkmarkColor: Colors.white, 
+  ); 
 }
---
+++ lib/utils/theme/custom_themes/outlined_button_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BOutlinedButtonTheme{
+  BOutlinedButtonTheme._();
+
+
+  static OutlinedButtonThemeData lightOutlinedButtonTheme = OutlinedButtonThemeData(
+    
+  );
+}
