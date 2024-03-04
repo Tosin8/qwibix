f572cf60fca7cb13bb229b3f870c4d3d41bb4d6c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-04 17:44:21 +0100

    Update device_utlity.dart

--- lib/utils/device/device_utlity.dart
+++ lib/utils/device/device_utlity.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 class BDeviceUtils {
   static void hideKeyboard(BuildContext context) {
@@ -14,7 +16,68 @@ class BDeviceUtils {
     
   }
 
-  static bool isTablet(BuildContext context) {
-    return MediaQuery.of(context).size.shortestSide >= 600;
+  static bool isLandscapeOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets; 
+    return viewInsets.bottom == 0; 
   }
+static bool isPortraitOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom != 0;
+}
+
+static void setFullScreen(bool enable) {
+  SystemChrome.setEnabledSystemUIMode(
+    enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge); 
+    
+}
+
+static double getScreenHeight() {
+  return MediaQuery.of(Get.context!).size.height; 
+}
+
+static double getScreenWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
+
+static double getPixelRatio() {
+  return MediaQuery.of(Get.context!).devicePixelRatio;
+}
+static double getStatusBarHeight() {
+  return MediaQuery.of(Get.context!).padding.top;
+}
+
+static double getBottomNavigationBarHeight() {
+  return kBottomNavigationBarHeight; 
+}
+
+static double getAppBarHeight() {
+  return kToolbarHeight;
+}
+static double getKeyboardHeight() {
+  final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+  return viewInsets.bottom; 
+}
+
+static Future<bool> isKeyboardVisible() async {
+  final viewInsets = View.of(Get.context!).viewInsets; 
+  return viewInsets.bottom> 0; 
+}
+
+static Future<bool> isPhysicalDevice() async{
+  return defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS;
+}
+
+static  void vibrate(Duration duration) {
+  HapticFeedback.vibrate(); 
+  Future.delayed(duration, () => HapticFeedback.vibrate());
+}
+
+static Future<void> setPreferredOrientations(List<DeviceOrientation> orientations) async {
+  await SystemChrome.setPreferredOrientations(orientations);
+}
+
+static void hideStatusBar() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+}
+
 }
