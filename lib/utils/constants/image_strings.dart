a2c7f155f7abbae19c4186158ad2fa6ded53dcdf
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-05 08:52:16 +0100

    Update image_strings.dart

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -6,4 +6,12 @@ class BImages {
   static const String facebook = '';
   static const String google = '';
   static const String apple = '';
+
+
+  // OnBoarding Images 
+  static const String onBoardingImage1 = 'assets/images/on_boarding/sammy-line-searching.gif'; 
+  static const String onBoardingImage2 = 'assets/images/on_boarding/sammy-line-shopping.gif'; 
+  static const String onBoardingImage3 = 'assets/images/on_boarding/sammy-line-delivery.gif'; 
+
+  // 
 }
