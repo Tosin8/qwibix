55eabbcc53ce4d6d75dae4c3cae4e0294c59c181
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-06 19:36:51 +0100

    Updating social media icons

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -4,8 +4,8 @@ class BImages {
 
   // social media
   static const String facebook = 'assets/icons/social_media/facebook.png';
-  static const String google = '';
-  static const String apple = '';
+  static const String google = 'assets/social_media/google.png';
+  
 
 
   // OnBoarding Images 
