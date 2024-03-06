401932346e68655706643fc68db69dad801311de
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-06 18:55:09 +0100

    Update login.dart

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:bellymax/common/styles/spacing_styles.dart';
+import 'package:bellymax/utils/constants/colors.dart';
 import 'package:bellymax/utils/constants/image_strings.dart';
 import 'package:bellymax/utils/constants/sizes.dart';
 import 'package:bellymax/utils/constants/text_strings.dart';
@@ -25,7 +26,7 @@ class LoginScreen extends StatelessWidget{
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Image(
-                    height: 150,
+                    height: 100,
                     image: AssetImage(dark ? BImages.darkApplogo : BImages.lightApplogo)), 
                     Text(BTexts.loginTitle, style: Theme.of(context).textTheme.headlineMedium,), 
                     const SizedBox(height: BSizes.sm), 
@@ -35,49 +36,78 @@ class LoginScreen extends StatelessWidget{
  const SizedBox(height: BSizes.spaceBtwInputFields,),
               /// Form
               Form(
-                child: Column(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: BSizes.spaceBtwSections),
+                  child: Column(
+                    children: [
+                      /// Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Iconsax.direct_right), 
+                            labelText: BTexts.email
+                        ),
+                      ), 
+                      const SizedBox(height: BSizes.spaceBtwInputFields,),
+                  
+                      /// Passwords
+                       TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Iconsax.password_check), 
+                            labelText: BTexts.password, suffixIcon: Icon(Iconsax.eye_slash)
+                        ),
+                        
+                      ),
+                      const SizedBox(height: BSizes.spaceBtwInputFields / 2,),
+                  
+                      /// Remember Me & Forget Password. 
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                  
+                          // Remember Me 
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: true, onChanged: (value) {
+                                  
+                                },
+                  
+                              ), 
+                              const Text(BTexts.rememberMe)
+                            ],
+                          ), 
+                          const SizedBox(height: BSizes.spaceBtwSections,),  
+                        ],
+                      ) , 
+                  
+                      /// Sign In Button 
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(onPressed: (){}, 
+                        child: const Text(BTexts.signIn)),
+                      ), 
+                  const SizedBox(height: BSizes.spaceBtwItems,),
+                      /// Create Account Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(onPressed: (){}, 
+                        child: const Text(BTexts.createAccount)),), 
+                        const SizedBox(height: BSizes.spaceBtwSections,),
+                    ],
+                  ),
+                ), 
+                ), 
+
+                /// Divider 
+                Row(
                   children: [
-                    /// Email
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(
-                          Iconsax.direct_right), 
-                          labelText: BTexts.email
-                      ),
-                    ), 
-                    const SizedBox(height: BSizes.spaceBtwInputFields,),
-
-                    /// Passwords
-                     TextFormField(
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(
-                          Iconsax.password_check), 
-                          labelText: BTexts.password, suffixIcon: Icon(Iconsax.eye_slash)
-                      ),
-                      
-                    ),
-                    const SizedBox(height: BSizes.spaceBtwInputFields / 2,),
-
-                    /// Remember Me & Forget Password. 
-                    Row(
-                      children: [
-
-                        // Remember Me 
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: true, onChanged: (value) {
-                                
-                              },
-
-                            ), 
-                            Text(BTexts.rememberMe)
-                          ],
-                        )
-                      ],
-                    ) 
-                  ],
-                ))
+                    
+                    Divider(color: dark ? BColors.darkGrey: BColors.grey,
+                    thickness: 0.5, indent: 60, endIndent: 5,)
+                  ]
+                )
             ],
           
           )
