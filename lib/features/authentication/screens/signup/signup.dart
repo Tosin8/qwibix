20e77e3bca4617f0250faaa452ece1be26f10528
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-06 20:10:58 +0100

    Update signup.dart

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -5,6 +5,12 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        
+      ),
+
+    );
   }
 }
