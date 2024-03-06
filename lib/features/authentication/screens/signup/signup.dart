10ccd87ca16fe259dbdeb91bc538d662fd8dd9be
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-06 20:37:00 +0100

    Update signup.dart

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -13,7 +13,7 @@ class SignUpScreen extends StatelessWidget {
       appBar: AppBar(),
       body:  SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(
+          padding: const EdgeInsets.all(
             BSizes.defaultSpace), 
           child: Column(
             children: [
@@ -21,7 +21,7 @@ class SignUpScreen extends StatelessWidget {
               /// title 
               Text(BTexts.signupTitle, 
               style: Theme.of(context).textTheme.headlineMedium,), 
-              SizedBox(height: BSizes.spaceBtwSections,), 
+              const SizedBox(height: BSizes.spaceBtwSections,), 
 
               /// Form
               Form(
@@ -32,24 +32,94 @@ Row(
   children: [
     Expanded(
       child: TextFormField(
+        textInputAction: TextInputAction.next, 
+        keyboardType: TextInputType.name,
+        
         expands: false,
-        decoration: InputDecoration(
+        decoration: const InputDecoration(
           labelText: BTexts.firstName, prefixIcon: Icon(Iconsax.user), 
         ),
       ),
     ), 
-    SizedBox(width: BSizes.spaceBtwInputFields,), 
+    const SizedBox(width: BSizes.spaceBtwInputFields,), 
       Expanded(
       child: TextFormField(
+         textInputAction: TextInputAction.next, 
+        keyboardType: TextInputType.name,
         expands: false,
-        decoration: InputDecoration(
+        decoration: const InputDecoration(
           labelText: BTexts.lastName, prefixIcon: Icon(Iconsax.user), 
         ),
       ),
     )
   ],
-)
+), 
+const SizedBox(height: BSizes.spaceBtwInputFields,), 
 
+///UserName
+TextFormField(
+         textInputAction: TextInputAction.next, 
+        keyboardType: TextInputType.name,
+      
+        decoration: const InputDecoration(
+          labelText: BTexts.username, prefixIcon: Icon(Iconsax.user), 
+        ),
+      ),
+      const SizedBox(height: BSizes.spaceBtwInputFields,), 
+
+///Email
+TextFormField(
+         textInputAction: TextInputAction.next, 
+        keyboardType: TextInputType.emailAddress,
+      
+        decoration: const InputDecoration(
+          labelText: BTexts.email, prefixIcon: Icon(Iconsax.direct), 
+        ),
+      ),
+      const SizedBox(height: BSizes.spaceBtwInputFields,), 
+
+///PhoneNumber
+TextFormField(
+         textInputAction: TextInputAction.next, 
+        keyboardType: TextInputType.phone,
+      
+        decoration: const InputDecoration(
+          labelText: BTexts.phoneNo, prefixIcon: Icon(Iconsax.call), 
+        ),
+      ),
+      const SizedBox(height: BSizes.spaceBtwInputFields,), 
+
+///Password
+TextFormField(
+         textInputAction: TextInputAction.next, 
+        keyboardType: TextInputType.visiblePassword,
+        obscureText: true,
+      
+        decoration: const InputDecoration(
+          labelText: BTexts.password, prefixIcon: Icon(Iconsax.password_check), suffixIcon: Icon(Iconsax.eye_slash) 
+        ),
+      ),
+      const SizedBox(height: BSizes.spaceBtwSections,), 
+
+      /// Terms and Conditions CheckBox 
+      Row(
+        children: [
+          SizedBox(
+            width: 24, height: 24, 
+            child: Checkbox(value: true, 
+            onChanged: (value){}),
+          ), 
+          const SizedBox(width: BSizes.spaceBtwItems,), 
+          const Text.rich(
+TextSpan(
+  children: 
+  [
+    TextSpan(text: '${BTexts.iAgreeTo}')
+  ]
+)
+          ), 
+        ],
+      )
                 ],
               ))
             ],
