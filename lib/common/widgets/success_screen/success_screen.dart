bf72a600abf5baa8d221a8e1e49e372626e21840
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-07 22:34:11 +0100

    Update success_screen.dart

---
+++ lib/common/widgets/success_screen/success_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
