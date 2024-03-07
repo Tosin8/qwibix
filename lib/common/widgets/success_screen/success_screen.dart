d116767e39ad0220b90a5e887efea51f484a3a68
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-07 22:49:17 +0100

    Update success_screen.dart

--- lib/common/widgets/success_screen/success_screen.dart
+++ lib/common/widgets/success_screen/success_screen.dart
@@ -1,6 +1,12 @@
 import 'package:bellymax/common/styles/spacing_styles.dart';
+import 'package:bellymax/features/authentication/screens/login/login.dart';
+import 'package:bellymax/utils/constants/image_strings.dart';
+import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:bellymax/utils/constants/text_strings.dart';
+import 'package:bellymax/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SuccessScreen extends StatelessWidget {
   const SuccessScreen({super.key});
@@ -11,7 +17,35 @@ class SuccessScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: BSpacingStyle.paddingWithAppBarHeight * 2, 
-          child: Column(),),
+          child: Column(
+            children: [
+              Image(image: const AssetImage(BImages.staticSuccessIllustration), 
+              width: BHelperFunctions.screenWidth() * 0.6,
+              ), 
+              const SizedBox(height: BSizes.spaceBtwSections,),
+
+              /// title and subtitle
+              Text(BTexts.yourAccountCreatedTitle, 
+              style: Theme.of(context).textTheme.headlineMedium,
+               textAlign: TextAlign.center,), 
+               const SizedBox(height: BSizes.spaceBtwItems,), 
+               
+                  Text(BTexts.yourAccountCreatedSubTitle, 
+              style: Theme.of(context).textTheme.labelMedium,
+               textAlign: TextAlign.center,),
+               const SizedBox(height: BSizes.spaceBtwSections,), 
+
+               /// Button 
+               SizedBox(
+                width: double.infinity, 
+                child: ElevatedButton(
+                  onPressed: (){
+                    Get.to(() => Get.to(const LoginScreen()));
+                  }, child: const Text(BTexts.tContinue)),
+               )
+
+            ],
+          ),),
 
       )
     );
