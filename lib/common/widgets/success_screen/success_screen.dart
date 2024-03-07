016a701961f6dd20f81ae56bf6da296494d3de35
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-07 22:34:56 +0100

    Update success_screen.dart

--- lib/common/widgets/success_screen/success_screen.dart
+++ lib/common/widgets/success_screen/success_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SuccessScreen extends StatelessWidget {
   const SuccessScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        
+      )
+    );
   }
 }
