b5c5b10477005490c1c3d11a72056335384eacbb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-07 22:33:30 +0100

    Create success screen

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,5 +1,6 @@
 import 'package:bellymax/utils/constants/image_strings.dart';
 import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:bellymax/utils/constants/text_strings.dart';
 import 'package:bellymax/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,32 @@ class VerifyEmailScreen extends StatelessWidget {
               width: BHelperFunctions.screenWidth() * 0.6,
               ), 
               const SizedBox(height: BSizes.spaceBtwSections,),
+
+              /// title and subtitle
+              Text(BTexts.confirmEmail, 
+              style: Theme.of(context).textTheme.headlineMedium,
+               textAlign: TextAlign.center,), 
+               const SizedBox(height: BSizes.spaceBtwItems,), 
+               Text('tosinezekiel8@gmail.com',
+                style: Theme.of(context).textTheme.labelLarge,textAlign: TextAlign.center), 
+                  const SizedBox(height: BSizes.spaceBtwItems,), 
+                  Text(BTexts.confirmEmailSubTitle, 
+              style: Theme.of(context).textTheme.labelMedium,
+               textAlign: TextAlign.center,),
+               const SizedBox(height: BSizes.spaceBtwSections,), 
+
+               ///Button
+               SizedBox(width: double.infinity, 
+               child: ElevatedButton(onPressed: (){},
+                child: Text(BTexts.tContinue), 
+                ),), 
+               const SizedBox(height: BSizes.spaceBtwItems,), 
+                SizedBox(width: double.infinity, 
+               child: TextButton(onPressed: (){},
+                child: Text(BTexts.resendEmail), 
+                ),), 
+
+
             ],
           ),),
       ),
