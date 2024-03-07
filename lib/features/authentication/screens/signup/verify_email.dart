e262d23a27c9d0ad72ba714d32968a189088e278
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-07 12:43:24 +0100

    Update verify_email.dart

---
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -0,0 +1,17 @@
+import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(BSizes.defaultSpace)),
+      ),
+    );
+  }
+}
