07f49b7093a38439a639605122731644f5163aed
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-07 13:13:43 +0100

    Update image_strings.dart

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -13,5 +13,12 @@ class BImages {
   static const String onBoardingImage2 = 'assets/images/on_boarding/sammy-line-shopping.gif'; 
   static const String onBoardingImage3 = 'assets/images/on_boarding/sammy-line-delivery.gif'; 
 
-  // 
+  // Animations 
+  static const String productsIllustrations = 'assets/images/animations/sammy-line-workout.gif';
+  static const String productsSaleIllustration = 'assets/images/animations/sammy-line-sale.png';
+  static const String staticSuccessIllustration = 'assets/images/animations/sammy-line-success.png';
+  static const String deliveredInPlaneIllustration = 'assets/images/animations/sammy-line-come-back-later.png';
+  static const String deliveredEmailIllustration = 'assets/images/animations/sammy-line-man-receives-a-mail.png';
+  static const String verifyIllustration = 'assets/images/animations/sammy-line-travel-backpack-with-passport-and-air-ticket.gif';
+
 }
