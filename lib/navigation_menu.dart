bb324e34d5e7c09ffb819b6c66a4577db2f55570
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-11 16:19:32 +0100

    Update navigation_menu.dart

---
+++ lib/navigation_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({Key? key}) : super(key: key);
+
+  @override 
+  Widget build(BuildContext  context) {
+    return Scaffold(); 
+  }
+}
