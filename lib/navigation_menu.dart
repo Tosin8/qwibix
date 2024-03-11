32738b4132cc3d86b6636af1f9617467d052f300
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-11 17:07:53 +0100

    Update navigation_menu.dart

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -6,20 +7,42 @@ class NavigationMenu extends StatelessWidget {
 
   @override 
   Widget build(BuildContext  context) {
+
+    final controller = Get.put(NavigationController()); 
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        elevation: 0,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Iconsax.home), 
-            label: 'Home'),
-       NavigationDestination(icon: Icon(Iconsax.shop), label: 'Menu'),
-          NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
-         
-          NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
-          
-          
-        ],),
+      bottomNavigationBar: Obx(
+        () =>
+         NavigationBar(
+          elevation: 0,
+        selectedIndex: controller.selectedIndex.value,
+        onDestinationSelected: ( index) =>
+          controller.selectedIndex.value = index,  
+        
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Iconsax.home), 
+              label: 'Home'),
+         NavigationDestination(icon: Icon(Iconsax.shop), label: 'Menu'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+           
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+            
+            
+          ],),
+      ),
+
+        body: controller.screens[controller.selectedIndex.value], 
     ); 
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    Container(color: Colors.green), 
+    Container(color: Colors.green), 
+    Container(color: Colors.green), 
+    Container(color: Colors.green), 
+  ]; 
 }
