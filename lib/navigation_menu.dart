dd9d643018d3a9e08a03b66cf49e2204683aaf50
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-11 16:34:58 +0100

    Update navigation_menu.dart

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({Key? key}) : super(key: key);
@@ -8,11 +9,16 @@ class NavigationMenu extends StatelessWidget {
     return Scaffold(
       bottomNavigationBar: NavigationBar(
         elevation: 0,
-        destinations: [
-          Container(color: Colors.green,), 
-          Container(color: Colors.blue,), 
-          Container(color: Colors.deepPurple,), 
-          Container(color: Colors.orange,), 
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Iconsax.home), 
+            label: 'Home'),
+       NavigationDestination(icon: Icon(Iconsax.shop), label: 'Menu'),
+          NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+         
+          NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          
+          
         ],),
     ); 
   }
