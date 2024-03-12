58d5ed43f8ab670d43cac07857226aedb13a489f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-12 19:15:28 +0100

    Update product_list.dart

--- lib/model/product_list.dart
+++ lib/model/product_list.dart
@@ -1,5 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class TodaySpecial {
   final String image;
-  final title; 
-  final description;
+  final String title; 
+  final String description;
+  final String brandTitle; 
+  final String price;
+  final String discount; 
+  TodaySpecial({   required this.title, 
+    required this.description, 
+    required this.brandTitle, 
+    required this.image,
+    required this.price,
+    required this.discount, 
+   
+    
+  });
+
 }
+
+
+final List TodaySpecialList = [
+  TodaySpecial(
+   title: 'Burger',
+  description: '',
+  brandTitle: '',
+   image: 'assets/images/menu/burger.jpg',
+   price: '\$10.00',
+   discount: '10%',
+  ),
+   
+]; 
