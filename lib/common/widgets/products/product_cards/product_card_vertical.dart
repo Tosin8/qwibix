e506adf0dda9b5cae826c176b9f75aa4894eaf4a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-12 18:20:58 +0100

    Update product_card_vertical.dart

---
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BProductCardVertical extends StatelessWidget {
+  const BProductCardVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
