8ee2966a8bce732a7744462b787d6671f68745ed
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-12 12:15:07 +0100

    Update home.dart

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:bellymax/model/menu_list.dart';
+import 'package:bellymax/utils/constants/colors.dart';
 import 'package:bellymax/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
@@ -14,17 +15,38 @@ class HomeScreen extends StatelessWidget {
     final darkMode = BHelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Iconsax.menu),
-        title: const Text('Belly Max'), centerTitle: true, 
+        leading:  Icon(Iconsax.menu, 
+        color: darkMode ? Colors.white : Colors.black,),
+        title: Text('Belly Max', 
+        style: TextStyle(
+          color: darkMode ? Colors.white: Colors.black
+        ) ,), centerTitle: true, 
         bottom: PreferredSize(
           preferredSize: const Size.fromHeight(60), child: BSearchController(darkMode: darkMode, 
           text: 'Search for dish or cuisine',),
           ), 
-        actions: const[
-          Icon(Iconsax.shopping_bag), 
-           SizedBox(width: 10,), 
-        
-          
+        actions: [
+          Stack(
+            children: [
+              Icon(Iconsax.shopping_bag, 
+            color: darkMode ? Colors.white : Colors.black,),
+            Positioned(
+              right: 0,
+          child: Container(
+                width: 18, height: 18, 
+                decoration: BoxDecoration(
+                  color: BColors.black, 
+                  borderRadius: BorderRadius.circular(100), 
+                ),
+                child: Center(
+                  child: Text('2',
+                  style: Theme.of(context).textTheme.labelLarge!.apply(color: BColors.white, fontSizeFactor: 0.8), 
+                  ), 
+                )
+              ),
+            ), 
+        ]), 
+           const SizedBox(width: 10,),       
         ],
       ),
       body: SingleChildScrollView(
@@ -43,7 +65,7 @@ class HomeScreen extends StatelessWidget {
             ],
             
           ),
-          SizedBox(height: 10,), 
+          const SizedBox(height: 10,), 
           Wrap(
             alignment: WrapAlignment.start,
             spacing: 20,
