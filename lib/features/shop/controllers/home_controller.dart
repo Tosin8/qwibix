0c4fee0cd17acd31550d051e8b4e5d2e4acd6233
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-12 16:32:03 +0100

    Update home_controller.dart

--- lib/features/shop/controllers/home_controller.dart
+++ lib/features/shop/controllers/home_controller.dart
@@ -1,3 +1,12 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController{}
+class HomeController extends GetxController{
+static HomeController get to => Get.find();
+
+final carouselCurrentIndex = 0.obs; 
+
+void updatePageIndicator(index) {
+  carouselCurrentIndex.value = index;
+}
+
+}
