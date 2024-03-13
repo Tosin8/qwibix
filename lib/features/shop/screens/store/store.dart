8606decab84ed124bba38e4078fc0aa78fbe711b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-13 19:21:37 +0100

    Update store.dart

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,11 +1,14 @@
 import 'package:bellymax/common/widgets/carticon.dart';
 import 'package:bellymax/common/widgets/images/b_brand_image.dart';
+import 'package:bellymax/common/widgets/texts/brandTitleText_withIcon.dart';
 import 'package:bellymax/common/widgets/texts/sectionHeading.dart';
 import 'package:bellymax/utils/constants/colors.dart';
+import 'package:bellymax/utils/constants/enums.dart';
 import 'package:bellymax/utils/constants/rounded_container.dart';
 import 'package:bellymax/utils/constants/sizes.dart';
 import 'package:bellymax/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Store extends StatelessWidget {
   const Store({super.key});
@@ -43,22 +46,38 @@ children: [
   // featured brand
  const BSectionHeading(title: 'Featured Partners', buttonTitle: ''), 
  const SizedBox(height: BSizes.spaceBtwItems / 1.5), 
- BRoundedContainer(
-  padding: const EdgeInsets.all(BSizes.sm), 
-  showBorder: true, 
-  backgroundColor: Colors.transparent, 
-  child: Row(
-    children: [
-
-      /// Icon 
-      BrandImageCard(
-        image: '',
-        isNetWorkImage: false, 
-        backgroundColor: Colors.transparent, 
-        overlayColor: BHelperFunctions.isDarkMode(context) ? BColors.white: BColors.black,), 
-        
-    ],
-  ),
+ GestureDetector( 
+  onTap: (){},
+   child: BRoundedContainer(
+    padding: const EdgeInsets.all(BSizes.sm), 
+    showBorder: true, 
+    backgroundColor: Colors.transparent, 
+    child: Row(
+      children: [
+   
+        /// Icon 
+        BrandImageCard(
+          image: '',
+          isNetWorkImage: false, 
+          backgroundColor: Colors.transparent, 
+          overlayColor: BHelperFunctions.isDarkMode(context) ? BColors.white: BColors.black,), 
+          SizedBox(width: BSizes.spaceBtwItems / 2,), 
+   
+          /// Text
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BrandTitleVerifyIcon(title: 'KFC', 
+              brandTextSize: TextSizes.large,), 
+              Text('256 products', 
+              overflow: TextOverflow.ellipsis, 
+              style: Theme.of(context).textTheme.labelMedium,), 
+            ],
+          )
+   
+      ],
+    ),
+   ),
  )
 ],
                   ),),
