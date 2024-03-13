5ffdc9bbb4671da7b5459135862570d5d8d4d54c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-13 18:20:08 +0100

    Update store.dart

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,7 +1,10 @@
-import 'package:bellymax/common/widgets/appbar.dart';
 import 'package:bellymax/common/widgets/carticon.dart';
+import 'package:bellymax/features/shop/screens/home/widgets/searchController.dart';
+import 'package:bellymax/utils/constants/colors.dart';
+import 'package:bellymax/utils/constants/sizes.dart';
 import 'package:bellymax/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Store extends StatelessWidget {
   const Store({super.key});
@@ -15,11 +18,37 @@ class Store extends StatelessWidget {
         title: Text('Menu', style: Theme.of(context).textTheme.headlineMedium,), 
         actions: [
           BCartCounterIcon(darkMode: darkMode), 
-          SizedBox(width: 10,)
+          const SizedBox(width: 10,)
         ],
       ),
       body: NestedScrollView(
-        headerSliverBuilder: SliverAppBar(), 
+        headerSliverBuilder: 
+        (_, innerBoxIsScrolled){
+          return[
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              pinned: true, 
+              floating: true, 
+              backgroundColor: BHelperFunctions.isDarkMode(context) ? BColors.black : BColors.white,
+              expandedHeight: 440, 
+              flexibleSpace:  Padding(
+                padding: EdgeInsets.all(
+                  BSizes.defaultSpace), 
+                  child: ListView(
+shrinkWrap: true, 
+physics: const NeverScrollableScrollPhysics(),
+children: [
+
+  // searchbar
+ 
+  BSearchController(darkMode: darkMode, text: ''), 
+  SizedBox(height: BSizes.spaceBtwSections,), 
+],
+                  ),),
+            ),
+          ]; 
+        }, 
+         
         body: Container())
     );
   }
