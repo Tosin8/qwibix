b11147ea98d36ac699a07b3ad8c46aedaa21a600
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-13 19:28:45 +0100

    Create grid_layout.dart

---
+++ lib/common/widgets/grid_layout/grid_layout.dart
@@ -0,0 +1,30 @@
+import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class GridLayout extends StatelessWidget {
+  const GridLayout({super.key, 
+  required this.itemCount, 
+  this.mainAxisExtent = 288, 
+  required this.itemBuilder});
+
+final int itemCount; 
+final double? mainAxisExtent; 
+final Widget? Function(BuildContext , int) itemBuilder; 
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder
+    (
+      itemCount: itemCount, 
+      shrinkWrap: true, 
+      padding: EdgeInsets.zero, 
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, 
+      mainAxisExtent: mainAxisExtent, 
+      mainAxisSpacing: BSizes.gridViewSpacing,
+      crossAxisSpacing: BSizes.gridViewSpacing, 
+       ),
+       itemBuilder: itemBuilder,
+    )
+    ;
+  }
+}
