0e4dd7df152fb85261ef45a867581cab6242ab5e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-13 19:14:37 +0100

    Update brandTitleText.dart

--- lib/common/widgets/texts/brandTitleText.dart
+++ lib/common/widgets/texts/brandTitleText.dart
@@ -18,7 +18,9 @@ final TextSizes brandTextSizes;
   @override
   Widget build(BuildContext context) {
     return Text(
-      
+      title, textAlign: textAlign, maxLines: maxLines, overflow: TextOverflow.ellipsis, 
+
+      style: brandTextSizes == TextSizes.small ? Theme.of(context).textTheme.labelMedium!.apply(color: color) : brandTextSizes == TextSizes.medium ? Theme.of(context).textTheme.bodyLarge!.apply(color: color) : Theme.of(context).textTheme.bodyMedium!.apply(color: color),
     );
   }
 }
