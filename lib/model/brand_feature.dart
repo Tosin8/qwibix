cc080fb095f90d9a4e023b52ae0e8ea414f020a4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-14 08:42:25 +0100

    Updating images

--- lib/model/brand_feature.dart
+++ lib/model/brand_feature.dart
@@ -14,22 +14,22 @@ class BrandFeature {
 final List BrandList = [
   BrandFeature(
     titleBrand: 'KFC', 
-    image: '', 
+    image: 'assets/images/brands/kfc.png', 
     productTitle: '88 Products'
   ), 
    BrandFeature(
     titleBrand: 'McDonald', 
-    image: '', 
+    image: 'assets/images/brands/mcdonald.png', 
     productTitle: '114 Products'
   ), 
    BrandFeature(
     titleBrand: 'Burger King', 
-    image: '', 
+    image: 'assets/images/brands/burger_king.png', 
     productTitle: '56 Products'
   ), 
    BrandFeature(
     titleBrand: 'Dominion Pizza', 
-    image: '', 
+    image: 'assets/images/brands/pizza.png', 
     productTitle: '106 Products'
   ), 
 ]; 
