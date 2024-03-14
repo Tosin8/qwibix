97bd596ab8399c9cd69ff7e33c7f9fda9c8d72e6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-14 15:24:24 +0100

    tabBar refactoring

---
+++ lib/common/widgets/products/product_cards/brandpartners.dart
@@ -0,0 +1,60 @@
+import 'package:bellymax/common/widgets/images/b_brand_image.dart';
+import 'package:bellymax/common/widgets/texts/brandTitleText_withIcon.dart';
+import 'package:bellymax/model/brand_feature.dart';
+import 'package:bellymax/utils/constants/enums.dart';
+import 'package:bellymax/utils/constants/rounded_container.dart';
+import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class BrandPartnersCard extends StatelessWidget {
+  const BrandPartnersCard({
+    super.key, required this.BrandList,
+  });
+
+final BrandFeature BrandList; 
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector( 
+     onTap: (){},
+      child: BRoundedContainer(
+       padding: const EdgeInsets.all(BSizes.sm), 
+       showBorder: true, 
+       backgroundColor: Colors.transparent, 
+       child: Row(
+         children: [
+      
+           /// Icon 
+           Flexible(
+             child: BrandImageCard(
+               image: BrandList.image,
+               isNetWorkImage: false, 
+               backgroundColor: Colors.transparent, 
+              //  overlayColor: BHelperFunctions.isDarkMode(context) ? BColors.white: BColors.black,
+              ),
+           ), 
+             const SizedBox(width: BSizes.spaceBtwItems / 2,), 
+      
+             /// Text
+             Expanded(
+               child: Column(
+                mainAxisSize: MainAxisSize.min,
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   BrandTitleVerifyIcon(
+                    
+                    title: BrandList.titleBrand, 
+                   brandTextSize: TextSizes.large,), 
+                   Text(BrandList.productTitle, 
+                   overflow: TextOverflow.ellipsis, 
+                   style: Theme.of(context).textTheme.labelMedium,), 
+                 ],
+               ),
+             )
+      
+         ],
+       ),
+      ),
+    );
+  }
+}
+
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,17 +1,13 @@
 import 'package:bellymax/common/widgets/carticon.dart';
 import 'package:bellymax/common/widgets/grid_layout/grid_layout.dart';
-import 'package:bellymax/common/widgets/images/b_brand_image.dart';
-import 'package:bellymax/common/widgets/texts/brandTitleText_withIcon.dart';
+import 'package:bellymax/common/widgets/products/product_cards/brandpartners.dart';
 import 'package:bellymax/common/widgets/texts/sectionHeading.dart';
 import 'package:bellymax/model/brand_feature.dart';
 import 'package:bellymax/utils/constants/colors.dart';
-import 'package:bellymax/utils/constants/enums.dart';
-import 'package:bellymax/utils/constants/rounded_container.dart';
 import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:bellymax/utils/device/device_utlity.dart';
 import 'package:bellymax/utils/helpers/helper_functions.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Store extends StatelessWidget {
   const Store({super.key});
@@ -19,115 +15,85 @@ class Store extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final darkMode = BHelperFunctions.isDarkMode(context);
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Menu', style: Theme.of(context).textTheme.headlineMedium,), 
-        actions: [
-          BCartCounterIcon(darkMode: darkMode), 
-          const SizedBox(width: 10,)
-        ],
+    return DefaultTabController( 
+      length: 8,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Menu', style: Theme.of(context).textTheme.headlineMedium,), 
+          actions: [
+            BCartCounterIcon(darkMode: darkMode), 
+            const SizedBox(width: 10,)
+          ],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: 
+          (_, innerBoxIsScrolled){
+            return[
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true, 
+                floating: true, 
+                backgroundColor: BHelperFunctions.isDarkMode(context) ? BColors.black : BColors.white,
+                expandedHeight: 300, 
+                flexibleSpace:  Padding(
+                  padding: const EdgeInsets.all(
+                    BSizes.defaultSpace), 
+                    child: ListView(
+      shrinkWrap: true, 
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+      
+        // featured brand
+       const BSectionHeading(title: 'Featured Partners', buttonTitle: ''), 
+       const SizedBox(height: BSizes.spaceBtwItems / 1.5), 
+      
+       GridLayout(itemCount: BrandList.length, mainAxisExtent: 80,
+       itemBuilder: (_, index) {
+        return BrandPartnersCard(BrandList: BrandList[index],);
+       }, 
+       ), 
+       
+      ],
+                    ),
+                    ),
+      
+                    // Tabs  for categories and brands
+                  bottom: BTabBar(), 
+              ),
+            ]; 
+          }, 
+           
+          body: Container())
       ),
-      body: NestedScrollView(
-        headerSliverBuilder: 
-        (_, innerBoxIsScrolled){
-          return[
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              pinned: true, 
-              floating: true, 
-              backgroundColor: BHelperFunctions.isDarkMode(context) ? BColors.black : BColors.white,
-              expandedHeight: 440, 
-              flexibleSpace:  Padding(
-                padding: const EdgeInsets.all(
-                  BSizes.defaultSpace), 
-                  child: ListView(
-shrinkWrap: true, 
-physics: const NeverScrollableScrollPhysics(),
-children: [
-
-  // featured brand
- const BSectionHeading(title: 'Featured Partners', buttonTitle: ''), 
- const SizedBox(height: BSizes.spaceBtwItems / 1.5), 
-
- GridLayout(itemCount: BrandList.length, mainAxisExtent: 80,
- itemBuilder: (_, index) {
-  return BrandPartnersCard(BrandList: BrandList[index],);
- })
- 
-],
-                  ),),
-
-                  // Tabs  for categories and brands
-                bottom: const TabBar(
-                  tabs: [
-                    Tab(child: Text('Salad'),), 
-                     Tab(child: Text('Burger'),), 
-                      Tab(child: Text('Soup'),), 
-                       Tab(child: Text('Chicken'),),  
-                       Tab(child: Text('Sea Food'),),
-                         Tab(child: Text('Drinks'),),  
-                         Tab(child: Text('Desserts'),), 
-                          Tab(child: Text('SandWish'),), 
-
-                  ]), 
-            ),
-          ]; 
-        }, 
-         
-        body: Container())
     );
   }
 }
 
-class BrandPartnersCard extends StatelessWidget {
-  const BrandPartnersCard({
-    super.key, required this.BrandList,
+class BTabBar extends StatelessWidget implements PreferredSizeWidget{
+  const BTabBar({
+    super.key, required this.tabs,
   });
 
-final BrandFeature BrandList; 
+final List<Widget> tabs; 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( 
-     onTap: (){},
-      child: BRoundedContainer(
-       padding: const EdgeInsets.all(BSizes.sm), 
-       showBorder: true, 
-       backgroundColor: Colors.transparent, 
-       child: Row(
-         children: [
-      
-           /// Icon 
-           Flexible(
-             child: BrandImageCard(
-               image: BrandList.image,
-               isNetWorkImage: false, 
-               backgroundColor: Colors.transparent, 
-               overlayColor: BHelperFunctions.isDarkMode(context) ? BColors.white: BColors.black,),
-           ), 
-             const SizedBox(width: BSizes.spaceBtwItems / 2,), 
-      
-             /// Text
-             Expanded(
-               child: Column(
-                mainAxisSize: MainAxisSize.min,
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                 children: [
-                   BrandTitleVerifyIcon(
-                    
-                    title: BrandList.titleBrand, 
-                   brandTextSize: TextSizes.large,), 
-                   Text(BrandList.productTitle, 
-                   overflow: TextOverflow.ellipsis, 
-                   style: Theme.of(context).textTheme.labelMedium,), 
-                 ],
-               ),
-             )
-      
-         ],
-       ),
-      ),
+    final dark = BHelperFunctions.isDarkMode(context); 
+    return Material(
+      color: dark ? BColors.black : BColors.white,
+      child: TabBar(
+        padding:  EdgeInsets.zero,
+        tabAlignment: TabAlignment.start, 
+        isScrollable: true, 
+        indicatorColor: BColors.primary, 
+        unselectedLabelColor: BColors.darkGrey, 
+        labelColor: BHelperFunctions.isDarkMode(context) ? BColors.white: BColors.primary,
+        tabs: tabs),
     );
   }
+  
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(BDeviceUtils.getAppBarHeight()); 
 }
 
