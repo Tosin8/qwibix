b2faa95eb6159b3f766b7a977400137477641bf3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-15 18:51:42 +0100

    Update userprofilemenu.dart

--- lib/features/shop/screens/profile/widget.profileuser/userprofilemenu.dart
+++ lib/features/shop/screens/profile/widget.profileuser/userprofilemenu.dart
@@ -1,24 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
 class userprofilemenu extends StatelessWidget {
   const userprofilemenu({
-    super.key,
+    super.key, required this.name, required this.middleName, required this.press,
   });
 
+final String name; 
+final String middleName; 
+final VoidCallback press; 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text('Name', 
-        style: Theme.of(context).textTheme.bodySmall,
-        overflow: TextOverflow.ellipsis,), 
-        Text('Tosin Ezekiel', 
-        style: Theme.of(context).textTheme.bodyMedium,
-        overflow: TextOverflow.ellipsis,),
-        Icon(Iconsax.arrow_right_34, size: 18) , 
-      ],
+    return GestureDetector(
+      onTap:press, 
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex:3, 
+            child: Text(name, 
+            style: Theme.of(context).textTheme.bodySmall,
+            overflow: TextOverflow.ellipsis,),
+          ), 
+          Expanded(
+            flex: 5,
+            child: Text(middleName, 
+            style: Theme.of(context).textTheme.bodyMedium,
+            overflow: TextOverflow.ellipsis,),
+          ),
+          const Expanded(child: Icon(Iconsax.arrow_right_34, size: 18)) , 
+        ],
+      ),
     );
   }
 }
