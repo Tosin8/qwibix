0e9d9f8ba27d86d751912303967254a9bd2f300f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-18 21:49:13 +0100

    Update signup_controller.dart

---
+++ lib/features/authentication/controllers/signup/signup_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+
+class SignupController extends GetxController{
+  static SignupController get instance => Get.find(); 
+
+  /// variables
+  /// 
+  ///  -- signup
+  /// 
+  Future<void> singup() async {
+    try{
+
+    } catch (e) {
+
+    }
+  finally {
+
+  }
+}}
