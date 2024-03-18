c91c5b6a01d301f7be442d8a7b654d565c117e6b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-18 21:53:02 +0100

    Update signup_controller.dart

--- lib/features/authentication/controllers/signup/signup_controller.dart
+++ lib/features/authentication/controllers/signup/signup_controller.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignupController extends GetxController{
   static SignupController get instance => Get.find(); 
 
   /// variables
+  final email = TextEditingController(); 
+  final lastName = TextEditingController(); 
+  final username = TextEditingController(); 
+  final password = TextEditingController(); 
+  final firstName = TextEditingController(); 
+  final phoneNumber = TextEditingController(); 
+  GlobalKey<FormState> signupFormKey = GlobalKey<FormState>(); //form key for form validation 
   /// 
   ///  -- signup
   /// 
