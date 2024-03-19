b95a72f0a32fdd8e6cac6ba44fe95ad5a1bf3dc0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-19 13:47:47 +0100

    Update loaders.dart

--- lib/common/widgets/loaders/loaders.dart
+++ lib/common/widgets/loaders/loaders.dart
@@ -1,3 +1,4 @@
+import 'package:bellymax/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -5,6 +6,26 @@ import 'package:iconsax/iconsax.dart';
 import '../../../utils/constants/colors.dart';
 
 class BLoaders {
+  static hideSnackBar() => ScaffoldMessenger.of(Get.context!).hideCurrentSnackBar(); 
+
+  static customToast({required message}) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(
+        elevation: 0, 
+        duration: const Duration(seconds: 3), 
+        backgroundColor: Colors.transparent,
+        content: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 30), 
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), 
+            color: BHelperFunctions.isDarkMode(Get.context!) ? BColors.darkerGrey.withOpacity(0.9) : BColors.grey.withOpacity(0.9), 
+          ),
+
+          child: Center(child: Text(message, style: Theme.of(Get.context!).textTheme.labelLarge,),),
+        ))
+    );
+  }
 
   static warningSnackBar ({
     required title, message = ''
