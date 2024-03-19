150c885029f18fbd784051472951178b930ff5cb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-19 15:22:53 +0100

    Update authentication_repository.dart

--- lib/features/authentication/controllers/authentication_repository.dart
+++ lib/features/authentication/controllers/authentication_repository.dart
@@ -1,7 +1,33 @@
+import 'package:bellymax/features/authentication/screens/login/login.dart';
+import 'package:bellymax/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class AuthenticationRepository extends GetxController{
   static AuthenticationRepository get instance => Get.find();
 
   /// variables
+  final deviceStorage = GetStorage(); 
+
+  /// Called from main.dart on app launch
+  @override 
+  void onReady() {
+    // Remove the native splash screen 
+    FlutterNativeSplash.remove(); 
+
+    // Redirect to the appropriate screen 
+    screenRedirect(); 
+  }
+
+  /// Function to show relevant screen 
+  screenRedirect() async{
+    // local storage
+    deviceStorage.writeIfNull('IsFirstTime', true); 
+    
+    // check if it's the first time launching the app
+    deviceStorage.read('IsFirstTime') != true ? Get.offAll(() => const LoginScreen()) : Get.offAll(() => const OnBoardingScreen());
+  }
+
+  /* _____________ Email and Password Validation _____________ */
 }
