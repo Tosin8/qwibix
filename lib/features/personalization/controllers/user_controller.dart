a285c82d79dec9f30963c1b60fad11f257a5684a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-20 22:45:17 +0100

    Update user_controller.dart

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -1,3 +1,4 @@
+import 'package:bellymax/common/widgets/loaders/loaders.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
@@ -7,5 +8,18 @@ class UserController extends GetxController {
 
  /// Save user record from any registration provider. 
  Future<void> saveUserRecord(UserCredential? userCredentials) async
- {}
+ {
+
+  try {
+if (userCredentials != null) {
+  
+}
+  } catch (e) 
+  {
+    BLoaders.warningSnackBar(
+      title: 'Data not found',
+       message: 'Something went wrong while saving your information.You can resave your data in your profile.',)
+       ; 
+  }
+ }
 }
