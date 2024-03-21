678b556892d272b844a761119d8d5db5c2250b18
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-21 16:58:48 +0100

    Update forget_password_controller.dart

--- lib/features/authentication/controllers/forget_pwd/forget_password_controller.dart
+++ lib/features/authentication/controllers/forget_pwd/forget_password_controller.dart
@@ -1,5 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ForgetPasswordController extends GetxController{
   static ForgetPasswordController get instance => Get.find();
+
+
+  /// variables
+  final email = TextEditingController(); 
+  GlobalKey<FormState> forgetPasswordFormKey = GlobalKey<FormState>(); 
+
+  /// send reset password email 
+  
+  sendPasswordResetEmail() async {
+    try{} catch (e) {}
+  }
+  
+  resendPasswordResetEmail(String email) async {
+    try {} catch (e) {}
+  }
 }
