423fabbdc8687c3cadc77022520f9c26b682ae5c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-22 19:38:14 +0100

    Update re_authenticate_user_login_form.dart

--- lib/features/shop/screens/profile/widget.profileuser/re_authenticate_user_login_form.dart
+++ lib/features/shop/screens/profile/widget.profileuser/re_authenticate_user_login_form.dart
@@ -1,6 +1,10 @@
 import 'package:bellymax/features/personalization/controllers/user_controller.dart';
 import 'package:bellymax/utils/constants/sizes.dart';
+import 'package:bellymax/utils/constants/text_strings.dart';
+import 'package:bellymax/utils/validators/validation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ReAuthLoginForm extends StatelessWidget {
   const ReAuthLoginForm({super.key});
@@ -12,10 +16,45 @@ class ReAuthLoginForm extends StatelessWidget {
       appBar: AppBar(title: const Text('Re-Authenticate User')),
       body:  SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(BSizes.defaultSpace), 
+          padding: const EdgeInsets.all(BSizes.defaultSpace), 
           child: Form(
             key: controller.reAuthFormKey, 
-            child: Column()),),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+// Email
+TextFormField(
+  textInputAction: TextInputAction.next,
+  controller: controller.verifyEmail, 
+  validator: BValidator.validateEmail, 
+  decoration: const InputDecoration(prefixIcon: Icon(Iconsax.direct_right), 
+  labelText: BTexts.email),
+), 
+const SizedBox(height: BSizes.spaceBtwInputFields,), 
+
+// Password
+Obx( 
+  () =>
+   TextFormField(
+    obscureText: controller.hidePassword.value,
+    textInputAction: TextInputAction.done,
+    controller: controller.verifyEmail, 
+    validator: BValidator.validateEmail, 
+    decoration:  InputDecoration(
+      suffixIcon: IconButton(onPressed: () => controller.hidePassword.value = !controller.hidePassword.value, icon: Icon(Iconsax.eye_slash)),
+      prefixIcon: Icon(Iconsax.password_check), 
+    labelText: BTexts.password),
+    
+  ),
+), 
+const SizedBox(height: BSizes.spaceBtwSections,), 
+
+// login btn 
+SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () => controller.reAuthenticateEmailAndPasswordUser(), child: const Text('Verify')),)
+              ],
+            ), 
+            ),
+            ),
       )
     );
   }
