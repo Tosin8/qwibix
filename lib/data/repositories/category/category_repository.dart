43949fd52325104cbbc9be1c71642e94d77630fb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-25 17:43:25 +0100

    Update category_repository.dart

--- lib/data/repositories/category/category_repository.dart
+++ lib/data/repositories/category/category_repository.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class CategoryRepository extends GetxController{
   static CategoryRepository get instance => Get.find(); 
+
+  /// Variables
+  final _db = FirebaseFirestore.instance; 
 }
