3f76a6e74c1795683d63b3c9e3edcf3d7de40a42
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-25 18:22:41 +0100

    Update category_controller.dart

--- lib/features/shop/controllers/category_controller.dart
+++ lib/features/shop/controllers/category_controller.dart
@@ -1,5 +1,29 @@
+import 'package:bellymax/common/widgets/loaders/loaders.dart';
+import 'package:bellymax/data/repositories/category/category_repository.dart';
+import 'package:bellymax/utils/popups/full_screen_loader.dart';
 import 'package:get/get.dart';
 
 class CategoryController extends GetxController {
   static CategoryController get instance => Get.find();
+
+  final _categoryRepository = Get.put(CategoryRepository()); 
+
+  @override 
+  void onInit() {
+    fetchCategories(); 
+    super.onInit(); 
+  }
+
+  /// Load category data
+  
+  Future<void>fetchCategories() async {
+    try{
+      
+    }
+    catch(e){
+      BLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    } finally {
+      BFullScreenLoader.stopLoading();
+    }
+  }
 }
