5e0955bfb9419774db11f2c9accdaf24cbbf7756
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-29 00:10:56 +0100

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,10 +1,21 @@
 import 'package:bellymax/features/shop/screens/home/home.dart';
+import 'package:bellymax/features/shop/screens/profile/profile.dart';
+import 'package:bellymax/features/shop/screens/wishlist/wishlist.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
+import '../features/shop/screens/store/store.dart';
 import 'routes.dart';
 
 class AppRoutes {
   static final pages = [
     GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
+     GetPage(name: BRoutes.store, page: () => const Store()), 
+      GetPage(name: BRoutes.favourites, page: () => const WishlistScreen()),
+        GetPage(name: BRoutes.settings, page: () => const ProfileScreen()), 
+         GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
+          GetPage(name: BRoutes.home, page: () => const HomeScreen()),  
+          GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
+           GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
   ]; 
 }
