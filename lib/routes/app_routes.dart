e161980efd75b8bca75c62ae441656a36a6295bb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-29 00:06:23 +0100

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,3 +1,10 @@
+import 'package:bellymax/features/shop/screens/home/home.dart';
+import 'package:get/get.dart';
+
+import 'routes.dart';
+
 class AppRoutes {
-  
+  static final pages = [
+    GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
+  ]; 
 }
