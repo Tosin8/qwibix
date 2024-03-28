b953fe743b2bdb948d18952ef9aa8287953a3f9e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-28 22:29:45 +0100

    banner repository

--- lib/features/shop/controllers/banner_controller.dart
+++ lib/features/shop/controllers/banner_controller.dart
@@ -1,15 +1,30 @@
 import 'package:get/get.dart';
 
+import '../../../common/widgets/loaders/loaders.dart';
+
 class BannerController extends GetxController{
 
   // variables
   final carouselCurrentIndex = 0.obs; 
+  final isLoading = false.obs; 
 
 // update page navigational dots
   void updatePageIndicator(index) {
     carouselCurrentIndex.value =index; 
   }
 
-  // fetch banners\
-  
+  // fetch banners
+  Future<void> fetchBanners() async {
+    try {
+      // show loader while loading categories. 
+      isLoading.value = true; 
+
+    } catch (e) {
+      BLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString()); 
+    } finally {
+      // remove loader
+      isLoading.value = false; 
+    }
+  }
+
 }
