1166d65d73b17911d1530ec4a4868d49afb339c9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-28 22:51:37 +0100

    Update dummy_data.dart

--- lib/data/services/dummy_data.dart
+++ lib/data/services/dummy_data.dart
@@ -1,5 +1,40 @@
+import 'package:bellymax/features/shop/models/menu_list.dart';
+
 class BDummyData {
 
-  /// Banners
-  static final List<BannerModel> banners = []; 
+  /// Category
+  static final List<CategoryModel> banners = [
+    CategoryModel(
+      id: '1', 
+       name: 'Burger', 
+       image: 'assets/images/menu/burger.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '2', 
+       name: 'Chicken', 
+       image: 'assets/images/menu/chicken.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '3', 
+       name: 'Desserts', 
+       image: 'assets/images/menu/desserts.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '4', 
+       name: 'Drinks', 
+       image: 'assets/images/menu/drinks.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '5', 
+       name: 'Salad', 
+       image: 'assets/images/menu/salad.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '6', 
+       name: 'SandWish', 
+       image: 'assets/images/menu/sandwish.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '7', 
+       name: 'Sea Food', 
+       image: 'assets/images/menu/sea_food.jpg', isFeatured: true), 
+       CategoryModel(
+      id: '8', 
+       name: 'Sour', 
+       image: 'assets/images/menu/soup.jpg', isFeatured: true), 
+  ]; 
 }
