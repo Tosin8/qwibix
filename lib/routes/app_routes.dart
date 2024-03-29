baed4f9368b30c5a5a3d69bb6ad06dc08fd51860
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-29 21:08:22 +0100

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,9 +1,13 @@
+import 'package:bellymax/features/authentication/screens/login/login.dart';
+import 'package:bellymax/features/authentication/screens/signup/signup.dart';
+import 'package:bellymax/features/authentication/screens/signup/verify_email.dart';
 import 'package:bellymax/features/shop/screens/home/home.dart';
 import 'package:bellymax/features/shop/screens/profile/profile.dart';
 import 'package:bellymax/features/shop/screens/wishlist/wishlist.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
+import '../features/shop/screens/profile/userscreen.dart';
 import '../features/shop/screens/store/store.dart';
 import 'routes.dart';
 
@@ -12,10 +16,10 @@ class AppRoutes {
     GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
      GetPage(name: BRoutes.store, page: () => const Store()), 
       GetPage(name: BRoutes.favourites, page: () => const WishlistScreen()),
-        GetPage(name: BRoutes.settings, page: () => const ProfileScreen()), 
-         GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
-          GetPage(name: BRoutes.home, page: () => const HomeScreen()),  
-          GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
-           GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
+        GetPage(name: BRoutes.userProfile, page: () => const ProfileScreen()), 
+         GetPage(name: BRoutes.settings, page: () => const ProfileUserScreen()), 
+          GetPage(name: BRoutes.signIn, page: () => const LoginScreen()),  
+          GetPage(name: BRoutes.signup, page: () => const SignUpScreen()), 
+           GetPage(name: BRoutes.verifyEmail, page: () => const VerifyEmailScreen()), 
   ]; 
 }
