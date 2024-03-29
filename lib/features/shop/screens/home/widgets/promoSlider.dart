1930ac6bea5e4ef51ca816b790f1ce57ef597bca
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-29 21:26:38 +0100

    Update promoSlider.dart

--- lib/features/shop/screens/home/widgets/promoSlider.dart
+++ lib/features/shop/screens/home/widgets/promoSlider.dart
@@ -1,5 +1,5 @@
 import 'package:bellymax/common/widgets/images/b_carousel_images.dart';
-import 'package:bellymax/features/shop/controllers/home_controller.dart';
+import 'package:bellymax/common/widgets/shimmer.dart';
 import 'package:bellymax/utils/constants/circular_container.dart';
 import 'package:bellymax/utils/constants/colors.dart';
 import 'package:bellymax/utils/constants/sizes.dart';
@@ -20,41 +20,45 @@ class BPromoSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(BannerController());
-    return Column(
-      children: [
-        CarouselSlider(
-         options: CarouselOptions(
-           viewportFraction: 1, 
-           onPageChanged: (index, _)=> controller.updatePageIndicator(index), 
-         ),
-        //  items: banners.map((url) => BCarouselImages(imageUrl: url)).toList(), 
-        items: controller.banners.map((banner) => BCarouselImages(imageUrl: banner.imageUrl, isNetworkImage: true,)).toList(), 
-        //  items: const [
-        //    BCarouselImages(imageUrl: BImages.banner1,),
-        //     BCarouselImages(imageUrl: BImages.banner2,),
-        //      BCarouselImages(imageUrl: BImages.banner3,),
-        //  ]
-         ),
-         const SizedBox(height: BSizes.spaceBtwItems,), 
-         Center(
-           child: Obx( ()=>
-              Row(
-                mainAxisSize: MainAxisSize.min,
-               children: [
-                //  for(int i = 0; i < banners.length; i++)
-                 for(int i = 0; i < controller.banners.length; i++)
-                  BCircularContainer(
-                   
-                   width: 20,
-                    height: 4,
-                    margin: const EdgeInsets.only(right: 10),
-                   backgroundColor: controller.carouselCurrentIndex.value == i ? BColors.primary: BColors.grey,),
-               ],
-             ),
+    return Obx( () {
+      // loader 
+      if(controller.isLoading.value) return const TShimmerEffect(width: double.infinity, height: 190); 
+      return Column(
+        children: [
+          CarouselSlider(
+           options: CarouselOptions(
+             viewportFraction: 1, 
+             onPageChanged: (index, _)=> controller.updatePageIndicator(index), 
            ),
-         )
-      ],
-    );
+          //  items: banners.map((url) => BCarouselImages(imageUrl: url)).toList(), 
+          items: controller.banners.map((banner) => BCarouselImages(imageUrl: banner.imageUrl, isNetworkImage: true,)).toList(), 
+          //  items: const [
+          //    BCarouselImages(imageUrl: BImages.banner1,),
+          //     BCarouselImages(imageUrl: BImages.banner2,),
+          //      BCarouselImages(imageUrl: BImages.banner3,),
+          //  ]
+           ),
+           const SizedBox(height: BSizes.spaceBtwItems,), 
+           Center(
+             child: Obx( ()=>
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                 children: [
+                  //  for(int i = 0; i < banners.length; i++)
+                   for(int i = 0; i < controller.banners.length; i++)
+                    BCircularContainer(
+                     
+                     width: 20,
+                      height: 4,
+                      margin: const EdgeInsets.only(right: 10),
+                     backgroundColor: controller.carouselCurrentIndex.value == i ? BColors.primary: BColors.grey,),
+                 ],
+               ),
+             ),
+           )
+        ],
+      );
+  });
   }
 }
 
