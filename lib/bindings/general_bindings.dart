74eed4bd7f1f6e3d84be7ba51fa11b7e77e2e907
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-01 23:47:05 +0100

    Update general_bindings.dart

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -2,9 +2,12 @@
 import 'package:get/get.dart';
 import 'package:qwibix/utils/http/network_manager.dart';
 
+import '../features/authentication/controllers/login/login_controller.dart';
+
 class GeneralBindings extends Bindings {
   @override
   void dependencies() {
     Get.put(NetworkManager()); 
+     Get.lazyPut(() => LoginController(), fenix: true);
   }
 }
