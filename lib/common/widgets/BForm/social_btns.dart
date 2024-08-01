19af8b872cfbc2cc0aae38d234ea1ca8e7814817
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-01 18:34:03 +0100

    Update social_btns.dart

--- lib/common/widgets/BForm/social_btns.dart
+++ lib/common/widgets/BForm/social_btns.dart
@@ -1,22 +1,77 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:qwibix/features/authentication/controllers/login/login_controller.dart';
+// import 'package:qwibix/utils/constants/colors.dart';
+// import 'package:qwibix/utils/constants/image_strings.dart';
+// import 'package:qwibix/utils/constants/sizes.dart';
+
+// class BFormSocialMedia extends StatelessWidget {
+//     const BFormSocialMedia({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = Get.put(LoginController());
+
+//     // Debug prints to check if the image paths are correct
+//     print('Google image path: ${BImages.google}');
+//     print('Facebook image path: ${BImages.facebook}');
+
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(color: BColors.grey),
+//             borderRadius: BorderRadius.circular(100),
+//           ),
+//           child: IconButton(
+//             onPressed: () => controller.googleSignIn(),
+//             icon: const Image(
+//               width: BSizes.iconMd,
+//               height: BSizes.iconMd,
+//               image: AssetImage(BImages.google),
+//             ),
+//           ),
+//         ),
+//         const SizedBox(
+//           width: BSizes.spaceBtwItems,
+//         ),
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(color: BColors.grey),
+//             borderRadius: BorderRadius.circular(100),
+//           ),
+//           child: IconButton(
+//             onPressed: () {},
+//             icon: const Image(
+//               width: BSizes.iconMd,
+//               height: BSizes.iconMd,
+//               image: AssetImage(BImages.facebook),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:qwibix/features/authentication/controllers/login/login_controller.dart';
 import 'package:qwibix/utils/constants/colors.dart';
 import 'package:qwibix/utils/constants/image_strings.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
 
+import '../../../features/authentication/controllers/login/login_controller.dart';
+
 class BFormSocialMedia extends StatelessWidget {
-    const BFormSocialMedia({
-    super.key,
-  });
+  const BFormSocialMedia({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(LoginController());
-
-    // Debug prints to check if the image paths are correct
-    print('Google image path: ${BImages.google}');
-    print('Facebook image path: ${BImages.facebook}');
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -28,16 +83,18 @@ class BFormSocialMedia extends StatelessWidget {
           ),
           child: IconButton(
             onPressed: () => controller.googleSignIn(),
-            icon: const Image(
+            icon: Image.asset(
+              BImages.google,
               width: BSizes.iconMd,
               height: BSizes.iconMd,
-              image: AssetImage(BImages.google),
+              errorBuilder: (context, error, stackTrace) {
+                print('Error loading Google image: $error');
+                return const Icon(Icons.error);
+              },
             ),
           ),
         ),
-        const SizedBox(
-          width: BSizes.spaceBtwItems,
-        ),
+        const SizedBox(width: BSizes.spaceBtwItems),
         Container(
           decoration: BoxDecoration(
             border: Border.all(color: BColors.grey),
@@ -45,10 +102,14 @@ class BFormSocialMedia extends StatelessWidget {
           ),
           child: IconButton(
             onPressed: () {},
-            icon: const Image(
+            icon: Image.asset(
+              BImages.facebook,
               width: BSizes.iconMd,
               height: BSizes.iconMd,
-              image: AssetImage(BImages.facebook),
+              errorBuilder: (context, error, stackTrace) {
+                print('Error loading Facebook image: $error');
+                return const Icon(Icons.error);
+              },
             ),
           ),
         ),
