da3069ec7a1ab30ddf2dbd7adb8131f95995a414
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-03 22:32:41 +0100

    Refactoring Auth Page SignIn

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:qwibix/common/styles/spacing_styles.dart';
-import 'package:qwibix/common/widgets/BForm/form_divider.dart';
-import 'package:qwibix/common/widgets/BForm/social_btns.dart';
-import 'package:qwibix/utils/constants/sizes.dart';
 import 'package:qwibix/utils/constants/text_strings.dart';
-import 'package:qwibix/utils/helpers/helper_functions.dart';
 
-
+import '../../../../common/widgets/BForm/form_divider.dart';
+import '../../../../common/widgets/BForm/social_btns.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/helpers/helper_functions.dart';
+import '../../controllers/login/login_controller.dart';
 import 'login.widget/login_form.dart';
 import 'login.widget/login_header.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-
-  // late LoginController controller;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller = Get.put(LoginController());
-  // }
-
-  // @override
-  // void dispose() {
-  //   Get.delete<LoginController>();
-  //   super.dispose();
-  // }
   @override
   Widget build(BuildContext context) {
     final dark = BHelperFunctions.isDarkMode(context);
+    Get.put(LoginController()); // Initialize the controller
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -43,25 +26,16 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              /// Logo, Title and SubTitle
               BLoginHeader(dark: dark),
-              const SizedBox(
-                height: BSizes.spaceBtwInputFields,
-              ),
-              /// Form
-               const BLoginForm(),
-
+              const SizedBox(height: BSizes.spaceBtwInputFields),
+              const BLoginForm(),
               BFormDivider(
-  dark: dark,
-  DividerText: (BTexts.orSignInWith.capitalize ?? 'Sign In With').toString(),
-  style: Theme.of(context).textTheme.labelMedium,
-),
-              const SizedBox(
-                height: BSizes.spaceBtwSections,
+                dark: dark,
+                DividerText: (BTexts.orSignInWith.capitalize ?? 'Sign In With').toString(),
+                style: Theme.of(context).textTheme.labelMedium,
               ),
-              /// Footer
-               const BFormSocialMedia(),
-               
+              const SizedBox(height: BSizes.spaceBtwSections),
+              const BFormSocialMedia(),
             ],
           ),
         ),
@@ -69,18 +43,3 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-
-
-
-              // Divider
-              // BFormDivider(
-              //   dark: dark,
-              //   DividerText: BTexts.orSignInWith.capitalize ?? 'Sign In With',
-              //   style: Theme.of(context).textTheme.labelMedium,
-              // ),
-
-              // biometrics.
-              //  const SizedBox(
-              //   height: BSizes.spaceBtwSections,
-              // ),
-              //  const Icon(Icons.fingerprint)
