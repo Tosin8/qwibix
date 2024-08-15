825e5b1e76030e66c36120b57f2dd4d19ea97881
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-15 23:01:22 +0100

    Refactoring Code

--- lib/features/authentication/screens/login/login.widget/login_form.dart
+++ lib/features/authentication/screens/login/login.widget/login_form.dart
@@ -16,11 +16,70 @@ class BLoginForm extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Form(
       key: controller.loginFormKey,
-      child: const Column(
-        children: [
+      child:  Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: BSizes.spaceBtwSections),
+        child: Column(
+          children: [
+        
+        // Email
+        TextFormField(
+          decoration: const InputDecoration(prefixIcon: Icon(Iconsax.direct_right), 
+          labelText: BTexts.email),
+          ), 
+          const SizedBox(height: BSizes.spaceBtwInputFields,), 
 
-        ],
-        ), 
+          // Password
+        TextFormField(
+          decoration: const InputDecoration(prefixIcon: Icon(Iconsax.password_check), 
+          labelText: BTexts.password, 
+          suffixIcon: Icon(Iconsax.eye_slash
+          ), 
+          ),
+          ), 
+          const SizedBox(height: BSizes.spaceBtwInputFields / 2,), 
+
+          // Remember Me and Forgot Password
+         Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+
+// Remember Me. 
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    Checkbox(value: true,
+     onChanged: (value){}), 
+     const Text(BTexts.rememberMe), 
+  ],
+), 
+
+// Forget Password. 
+TextButton(
+  onPressed: (){}, 
+  child: const Text(BTexts.forgetPassword
+  ), 
+  ), 
+            ],
+          ), 
+
+          const SizedBox(height: BSizes.spaceBtwSections,), 
+
+          // Sign In Button
+          SizedBox(
+            width: double.infinity, 
+            child: ElevatedButton(
+              onPressed: (){},
+
+           child: const Text(BTexts.signIn)),), 
+const SizedBox(height: BSizes.spaceBtwItems,), 
+           // Create Account
+           SizedBox(width: double.infinity, child: OutlinedButton(onPressed: (){}, child: const Text(BTexts.createAccount))), 
+           const SizedBox(height: BSizes.spaceBtwSections)
+          ],
+          ),
+      ), 
   
     );   
   }
