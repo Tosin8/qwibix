c1df1dfa15d0352661011ffaa7ef1e1128c5527f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-16 10:14:11 +0100

    Update promo_slider.dart

--- lib/features/shop/screens/home/widgets/promo_slider.dart
+++ lib/features/shop/screens/home/widgets/promo_slider.dart
@@ -1,8 +1,9 @@
 
-import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:qwibix/common/widgets/images/b_rounded_image.dart';
+import 'package:qwibix/features/shop/controllers/home_controller.dart';
 import 'package:qwibix/utils/constants/image_strings.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
 
@@ -15,12 +16,15 @@ class BPromoSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final controller = Get.put(HomeController());
     return Column(
       children: [
         CarouselSlider
         
         (
-          options: CarouselOptions(viewportFraction: 1)
+          options: CarouselOptions(viewportFraction: 1, 
+          onPageChanged: (index, _) => controller.updatePageIndicator(index),)
           ,
           items: const [
                 BRoundedImage(imageUrl: BImages.banner2,), 
