2e1e3a45c4be1ac555c544107f2206c4bdc7d88e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-16 03:59:05 +0100

    creating custom_shapes

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:qwibix/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:qwibix/utils/constants/circular_container.dart';
 
 import 'package:qwibix/utils/constants/colors.dart';
@@ -13,15 +14,31 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: BColors.primary, 
-              padding: const EdgeInsets.all(0), 
-              child:  Stack(
-                children: [
-                  BCircularContainer(
-                    backgroundColor: BColors.textWhite.withOpacity(0.1),
-                  )
-                ],
+            ClipPath( 
+              clipper: BCustomCurvedEdges(),
+              child: Container(
+                color: BColors.primary, 
+                padding: const EdgeInsets.all(0), 
+                child:  SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150, right: -250, 
+                        child: BCircularContainer(
+                          
+                          backgroundColor: BColors.textWhite.withOpacity(0.1),
+                        ),
+                      ),
+                       Positioned(
+                        top: 100, right: -300, 
+                         child: BCircularContainer(
+                          backgroundColor: BColors.textWhite.withOpacity(0.1),
+                                           ),
+                       ),  
+                    ],
+                  ),
+                ),
               ),
             )
           ],
