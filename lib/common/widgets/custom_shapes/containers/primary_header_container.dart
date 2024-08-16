83ab06eb35d66a03abfe627bbd1fefe108761625
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-16 04:15:55 +0100

    Rfactoring

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/colors.dart';
+import '../curved_edges/curved_edges_widget.dart';
+import 'circular_container.dart';
+
+class BPrimaryHeaderContainer extends StatelessWidget {
+  const BPrimaryHeaderContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+final Widget child; 
+  @override
+  Widget build(BuildContext context) {
+    return BCurvedEdgeWidget(
+      child:  Container(
+            color: BColors.primary, 
+            padding: const EdgeInsets.all(0), 
+            child:  SizedBox(
+              height: 400,
+              child: Stack(
+    children: [
+      Positioned(
+        top: -150, right: -250, 
+        child: BCircularContainer(
+          
+          backgroundColor: BColors.textWhite.withOpacity(0.1),
+        ),
+      ),
+       Positioned(
+        top: 100, right: -300, 
+         child: BCircularContainer(
+          backgroundColor: BColors.textWhite.withOpacity(0.1),
+                           ),
+       ),  
+    ],
+              ),
+            ),
+          ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,45 +1,22 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-import 'package:qwibix/utils/constants/circular_container.dart';
-import 'package:qwibix/utils/constants/colors.dart';
 
-import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return const Scaffold( 
       body: SingleChildScrollView(
         child: Column(
           children: [
-            BCurvedEdgeWidget(
-              child:  Container(
-        color: BColors.primary, 
-        padding: const EdgeInsets.all(0), 
-        child:  SizedBox(
-          height: 400,
-          child: Stack(
-            children: [
-              Positioned(
-                top: -150, right: -250, 
-                child: BCircularContainer(
-                  
-                  backgroundColor: BColors.textWhite.withOpacity(0.1),
-                ),
+            BPrimaryHeaderContainer(
+              child: Column(
+                children: [],
               ),
-               Positioned(
-                top: 100, right: -300, 
-                 child: BCircularContainer(
-                  backgroundColor: BColors.textWhite.withOpacity(0.1),
-                                   ),
-               ),  
-            ],
-          ),
-        ),
-      ),
             )
           ],
         ),
