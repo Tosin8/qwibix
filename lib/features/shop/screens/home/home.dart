5d0fbafdbca46f6f4e4cc0a3788bcef4e4997e73
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-16 05:55:29 +0100

    Update home.dart

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:qwibix/utils/constants/colors.dart';
+import 'package:qwibix/utils/constants/sizes.dart';
+import 'package:qwibix/utils/device/device_utlity.dart';
 
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
@@ -18,7 +22,16 @@ class HomeScreen extends StatelessWidget {
               BPrimaryHeaderContainer(
                 child: Column(
                   children: [
-                    BHomeAppBar()
+
+                    // appbar
+                    BHomeAppBar(), 
+
+                    SizedBox(height: BSizes.spaceBtwSections,),
+
+                    // search bar
+                    BSearchContainer(), 
+
+                    // categories
                   ],
                 ),
               )
@@ -27,6 +40,31 @@ class HomeScreen extends StatelessWidget {
         )
       ),
     );
+  }
+}
+
+class BSearchContainer extends StatelessWidget {
+  const BSearchContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: BSizes.defaultSpace),
+      child: Container(
+        
+        width: BDeviceUtils.getScreenWidth(context),
+        padding: const EdgeInsets.all(BSizes.md), 
+        decoration: BoxDecoration(color: BColors.white,borderRadius: BorderRadius.circular(BSizes.cardRadiusLg),
+         border: Border.all(color: BColors.grey)),
+      child: Row(
+        children: [
+          const Icon(Iconsax.search_normal, color: BColors.darkGrey), 
+          const SizedBox(width: BSizes.spaceBtwItems,), 
+          Text('Search', style: Theme.of(context).textTheme.bodySmall),
+        ],
+      ),),);
   }
 }
 
