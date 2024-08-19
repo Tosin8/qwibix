e9ebe4b809dda57ebfc005a4774f2763670dac0c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-19 19:30:08 +0100

    Create product_detail.dart

---
+++ lib/features/shop/screens/home/product_details/widget/product_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold( 
+      body: SingleChildScrollView(
+        child: Column(),
+      )
+    );
+  }
+}
