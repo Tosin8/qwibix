90667fa92432898d5d4039de044b47faadd29b2f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-19 20:48:34 +0100

    Update product_meta_data.dart

---
+++ lib/features/shop/screens/home/product_details/widget/product_meta_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:qwibix/utils/helpers/helper_functions.dart';
+
+class ProductMetaData extends StatelessWidget {
+  const ProductMetaData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = BHelperFunctions.isDarkMode(context);
+    return const Column( 
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [],
+    );
+  }
+}
