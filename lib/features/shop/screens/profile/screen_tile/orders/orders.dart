793ebc7ea3d384293fa7438bac24235841ce56e7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-21 00:45:24 +0100

    create orderlistitems

--- lib/features/shop/screens/profile/screen_tile/orders/orders.dart
+++ lib/features/shop/screens/profile/screen_tile/orders/orders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qwibix/common/widgets/appbar/appbar.dart';
+import 'package:qwibix/features/shop/screens/profile/screen_tile/orders/widget/order_list.dart';
+import 'package:qwibix/utils/constants/sizes.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
@@ -10,7 +12,14 @@ class OrderScreen extends StatelessWidget {
       appBar: BAppBar(
         showBackArrow: true,
         title: Text('My Orders',
-        style: Theme.of(context).textTheme.headlineSmall,),)
+        style: Theme.of(context).textTheme.headlineSmall,),), 
+
+        body: const Padding(padding: EdgeInsets.all(BSizes.defaultSpace), 
+        
+        // Orders
+        child: OrderListItems(), 
+        
+        ),
     );
   }
 }
---
+++ lib/features/shop/screens/profile/screen_tile/orders/widget/order_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderListItems extends StatelessWidget {
+  const OrderListItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}
