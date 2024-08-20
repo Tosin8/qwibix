756f05fb67d4f9137ced5f083d100e022817ae7c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-20 20:54:25 +0100

    Create my_address.dart

---
+++ lib/features/shop/screens/profile/screen_tile/my_address.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserAddressScreen extends StatelessWidget {
+  const UserAddressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
