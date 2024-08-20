c622ccf61323ecf752575cd996be6772aa8930aa
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-20 20:08:57 +0100

    create overall product rating widgets

--- lib/features/shop/screens/home/product_reviews/product_reviews.dart
+++ lib/features/shop/screens/home/product_reviews/product_reviews.dart
@@ -4,45 +4,30 @@ import 'package:flutter/material.dart';
 import 'package:qwibix/common/widgets/appbar/appbar.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
 
-import 'widgets/rating_indicator.dart';
+import 'widgets/overall_product_rating.dart';
 
 class ProductReviewsScreen extends StatelessWidget {
   const ProductReviewsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return const Scaffold( 
 
       // appbar
-      appBar: const BAppBar(
+      appBar: BAppBar(
         title: Text('Product Reviews & Ratings'),showBackArrow: true,),
       body: SingleChildScrollView(
         child: Padding(padding: 
-        const EdgeInsets.all(BSizes.defaultSpace),
+        EdgeInsets.all(BSizes.defaultSpace),
         
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Rating and Reviews are verified and are from people who use the  same type of device that you use',), 
-            const SizedBox(height: BSizes.spaceBtwItems,), 
+            Text('Rating and Reviews are verified and are from people who use the  same type of device that you use',), 
+            SizedBox(height: BSizes.spaceBtwItems,), 
 
             // product product ratings. 
-Row(
-  children: [
-    Expanded(
-      flex: 3, 
-      child: Text('4.8', style: Theme.of(context).textTheme.displayLarge,)),
-    const Expanded(
-      flex: 7,
-      child: Column(
-        children: [
-          RatingProgressIndicator(text: '5', value: 1.0,), 
-          RatingProgressIndicator(text: '4', value: 0.8,), RatingProgressIndicator(text: '3', value: 0.6,), RatingProgressIndicator(text: '2', value: 0.4,), RatingProgressIndicator(text: '1', value: 0.2,), 
-        ],
-      ),
-    )
-  ],
-)
+OverallProductRating()
 
           ],
         ),
