c1236047d3decf4ecaca5c10e63997432778b543
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-21 05:05:13 +0100

    Updt

--- lib/features/shop/screens/extensions/brands/all_brand.dart
+++ lib/features/shop/screens/extensions/brands/all_brand.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:qwibix/common/widgets/appbar/appbar.dart';
 import 'package:qwibix/common/widgets/grid_layout/grid_layout.dart';
 import 'package:qwibix/common/widgets/products/product_cards/brand_card.dart';
 import 'package:qwibix/common/widgets/texts/sectionHeading.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
+
+import 'brand_products.dart';
 
 class AllBrandsScreen extends StatelessWidget {
   const AllBrandsScreen({super.key});
@@ -26,7 +29,7 @@ class AllBrandsScreen extends StatelessWidget {
             // Bramds
             GridLayout(itemCount: 6, mainAxisExtent: 80,
             
-             itemBuilder: (_, index) => const BrandCard(showBorder: true,),)
+             itemBuilder: (_, index) => BrandCard(showBorder: true, onTap: () => Get.to(() => const BrandProducts()),),)
           ],
         ),
         ), 
--- lib/features/shop/screens/extensions/brands/brand_products.dart
+++ lib/features/shop/screens/extensions/brands/brand_products.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:qwibix/common/widgets/appbar/appbar.dart';
+import 'package:qwibix/common/widgets/products/product_cards/brand_card.dart';
+import 'package:qwibix/common/widgets/products/sortable.dart';
+import 'package:qwibix/utils/constants/sizes.dart';
 
 class BrandProducts extends StatelessWidget {
   const BrandProducts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold( 
+      appBar: BAppBar(
+        title: Text('Nike'), showBackArrow: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(padding: 
+        EdgeInsets.all(BSizes.defaultSpace),
+        child: Column(
+          children: [
+
+            // Brand Detail 
+            BrandCard(showBorder: true), 
+            SizedBox(height: BSizes.spaceBtwSections,), 
+
+            SortableProducts(), 
+          ],
+        ),
+        ),
+      ),
+    );
   }
 }
