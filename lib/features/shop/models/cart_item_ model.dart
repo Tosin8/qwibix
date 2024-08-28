b52d1ddca74f69e05f55fd2e57631d991f073e10
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-29 00:06:06 +0100

    Update cart_item_ model.dart

--- lib/features/shop/models/cart_item_ model.dart
+++ lib/features/shop/models/cart_item_ model.dart
@@ -5,8 +5,24 @@ class CartItemModel{
   String? image; 
   String variationId; 
   String? brandName; 
+  int quantity; 
   Map<String, String>? selectedVariation; 
-}
+
 
 
 // constructor. 
+CartItemModel({
+  required this.productId, 
+  this.title = '', 
+required this.quantity, 
+  this.image,
+  this.variationId = '',
+    this.brandName, 
+    this.selectedVariation, 
+    this.price = 0.0,
+    });
+
+    // Empty Cart. 
+    static CartItemModel empty() => CartItemModel( productId: '', quantity: 0); 
+
+}
