835812cb4fd1362fd036da2fccaa471ffe34a1a5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-29 01:01:41 +0100

    Update banner_model.dart

--- lib/features/shop/models/banner_model.dart
+++ lib/features/shop/models/banner_model.dart
@@ -5,18 +5,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BannerModel {
   String imageUrl;
-  final String targetScreen; 
+  //final String targetScreen; 
   final bool active; 
   BannerModel({
     required this.imageUrl,
-    required this.targetScreen,
+ //   required this.targetScreen,
     required this.active,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'ImageUrl' : imageUrl, 
-      'TargetScreen' : targetScreen, 
+      //'TargetScreen' : targetScreen, 
       'Active': active, 
     }; 
   }
@@ -25,7 +25,7 @@ class BannerModel {
     final data = snapshot.data() as Map<String, dynamic>; 
     return BannerModel(
       imageUrl: data['ImageUrl'] ?? '',
-     targetScreen: data['TargetScreen'] ?? '', 
+   // targetScreen: data['TargetScreen'] ?? '', 
      active: data['Active'] ?? false, 
      ); 
   }
