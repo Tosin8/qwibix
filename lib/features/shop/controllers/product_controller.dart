e45de812f6ecf29f27032e6167c0ff5fce03873a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-31 21:41:27 +0100

    Update product_controller.dart

--- lib/features/shop/controllers/product_controller.dart
+++ lib/features/shop/controllers/product_controller.dart
@@ -2,4 +2,12 @@ import 'package:get/get.dart';
 
 class ProductController  extends GetxController{
   static ProductController get instance => Get.find(); 
+
+  RxList<ProductModel> featuredProducts = <ProductModel>[].obs; 
+
+  @override
+  void initState() {
+    super.initState();
+    
+  }
 }
