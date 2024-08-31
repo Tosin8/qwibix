1712ed2632fd431688c3709ab1ce542bde271278
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-31 20:59:12 +0100

    Update product_model.dart

--- lib/features/shop/models/product_model.dart
+++ lib/features/shop/models/product_model.dart
@@ -69,8 +69,27 @@ static ProductModel empty() => ProductModel(id: '',
  }
 
  // Map json oriented document snapshot from firebase to Model.
- factory ProductModel.fromQuerySnapshot(QueryDocumentSnapshot<Object?> document){
-  final data = document.data() as Map<String, dynamic>; 
-  return ProductModel(id: id, stock: stock, price: price, title: title, thumbnail: thumbnail, productType: productType); 
+ factory ProductModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document){
+  final data = document.data()!; 
+  return ProductModel(
+    id: document.id, 
+    sku: data['SKU'], 
+    stock: data['Stock'] ?? 0,
+    isFeatured: data['IsFeatured'] ?? false,
+     price: double.parse((data['Price'] ?? 0.0).toString()),
+     salePrice: double.parse((data['SalePrice'] ?? 0.0).toString()), 
+     title: data['Title'], 
+     thumbnail: data['Thumbnail'] ?? '', 
+     categoryId: data['CategoryId'] ?? '', 
+     description: data['Description'] ?? '', 
+
+      productType: data['ProductType'] ?? '', 
+      brand: BrandModel.fromJson(data['Brand']), 
+      images: data['Images']  != null ? List<String>.from(data['Images']): [], 
+      productAttributes: (data['ProductAttributes'] as List<dynamic>).map((e) => ProductAttributeModel.fromJson(e)).toList(), 
+      productVariations: (data['ProductVariations'] as List<dynamic>).map((e) => ProductVariationModel.fromJson(e)).toList(), 
+      ); 
  }
+
+ // Map json-oriented document snapshot from firebase to Model 
 }
