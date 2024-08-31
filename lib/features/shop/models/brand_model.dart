19ca7486b027e8eb14f312ead47983c73547f193
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-31 20:02:31 +0100

    Update brand_model.dart

--- lib/features/shop/models/brand_model.dart
+++ lib/features/shop/models/brand_model.dart
@@ -4,7 +4,36 @@ class BrandModel {
   String image; 
   bool? isFeatured; 
   int? productsCount; 
-  BrandModel({required this.id,required this.name,required this.image,this.isFeatured,required this.productsCount});
+  BrandModel({required this.id,required this.name,required this.image,this.isFeatured,
+  this.productsCount});
 
-  
+
+  // Empty Helper function.
+  static BrandModel empty() => BrandModel(id: '', 
+  name: '', image: '', 
+  ); 
+
+  // convert model to json structure for starting data in firebase. 
+  toJson() {
+    return {
+      'Name': name,
+      'Id': id,
+      'Image': image,
+      'IsFeatured': isFeatured,
+      'ProductsCount': productsCount
+    };
+  }
+
+  /// Map json oriented document snapshot from firebase to usermodel.
+  factory BrandModel.fromJson(Map<String, dynamic> document) {
+    final data = document; 
+    if (data.isEmpty) return BrandModel.empty();
+    return BrandModel(
+      id: data['Id'] ?? '',
+      name: data['Name'] ?? '',
+      image: data['Image'] ?? '',
+      isFeatured: data['IsFeatured'] ?? '',
+      productsCount: data['ProductsCount'] ?? '',
+    );
+  }
 }
