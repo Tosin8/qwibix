a4b50ba838537e113ed66d86db87fa8db5c01e7d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-31 20:10:43 +0100

    Create product variation model

--- lib/features/shop/models/product_attribute_model.dart
+++ lib/features/shop/models/product_attribute_model.dart
@@ -1,3 +1,24 @@
 class ProductAttributeModel {
-  
+  String? name; 
+  final List<String>? values; 
+  ProductAttributeModel({this.name, this.values});
+
+  // Json Format
+  toJson() {
+    return {
+      'Name': name, 
+      'Values': values
+    }; 
+  }
+
+  // Map Json oriented document snapshot from firebase to Model
+  factory ProductAttributeModel.fromJson(Map<String, dynamic> document){
+    final data = document; 
+
+    if(data.isEmpty) return ProductAttributeModel(); 
+    return ProductAttributeModel(
+      name: data.containsKey('Name')? data['Name']: '',
+      values: List<String>.from(data['Values']), 
+    );
+  }
 }
