87587d541dbff3f9fb14fff97088c8de77fe59a5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-02 12:39:41 +0100

    Update product_detail_image_slider.dart

--- lib/features/shop/screens/home/product_details/widget/product_detail_image_slider.dart
+++ lib/features/shop/screens/home/product_details/widget/product_detail_image_slider.dart
@@ -1,13 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import 'package:qwibix/common/widgets/appbar/appbar.dart';
 import 'package:qwibix/common/widgets/images/b_rounded_image.dart';
+import 'package:qwibix/features/shop/controllers/product/images_controller.dart';
 import 'package:qwibix/utils/constants/circular_icon.dart';
 import 'package:qwibix/utils/constants/colors.dart';
-import 'package:qwibix/utils/constants/image_strings.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
 import 'package:qwibix/utils/helpers/helper_functions.dart';
 
@@ -20,62 +22,72 @@ class ProductImageSlider extends StatelessWidget {
     required this.product,
   }) : super(key: key);
 
-  final ProductModel product; 
+  final ProductModel product;
 
   @override
   Widget build(BuildContext context) {
-    final dark = BHelperFunctions.isDarkMode(context); 
+    final dark = BHelperFunctions.isDarkMode(context);
+    final controller = Get.put(ImagesController());
+    final images = controller.getAllProductImages(product);
     return BCurvedEdgeWidget(
-      child: Container( color: dark ? BColors.darkerGrey : BColors.light,
-      child: Stack(
-        children: [
-    
-          // Main Image Large 
-          const SizedBox(
-            height: 400, 
-            child: Padding(
-              padding: EdgeInsets.all(BSizes.productImageRadius * 2
-              ), 
-              child: Center(child: Image(image: AssetImage(BImages.productImage1))),
-            ),
-          ), 
-    
-          // Image Slider. 
-    
-          Positioned( 
-            right: 0, 
-            bottom: 30, 
-            left: BSizes.defaultSpace, 
-            child: SizedBox(
-              height: 80 , 
-              child: ListView.separated(
-                itemCount: 4, 
-                shrinkWrap: true, 
-                scrollDirection: Axis.horizontal,
-                physics: const AlwaysScrollableScrollPhysics(),
-                separatorBuilder: (_, __) => const SizedBox(width: BSizes.spaceBtwItems,),
-                itemBuilder: (_, index) => BRoundedImage(
-                width: 80, 
-                backgroundColor: dark ? BColors.dark : BColors.white,
-                imageUrl: BImages.productImage1, 
-                border: Border.all(color: BColors.primary),
-                padding: const EdgeInsets.all(BSizes.sm), 
-                ), 
+      child: Container(
+          color: dark ? BColors.darkerGrey : BColors.light,
+          child: Stack(
+            children: [
+              // Main Image Large
+              SizedBox(
+                height: 400,
+                child: Padding(
+                  padding: const EdgeInsets.all(BSizes.productImageRadius * 2),
+                  child: Center(
+                      child: Obx(() {
+                        final image = controller.selectedProductImage.value; 
+                        return
+                          CachedNetworkImage(imageUrl: image, 
+                          progressIndicatorBuilder: (_, __, downloadProgress) => CircularProgressIndicator(value: downloadProgress.progress, color: BColors.primary),
+                          ); 
+                      }
+                          )
+                          ),
+                ),
               ),
-            ),
-          ), 
-    
-          // AppBar Icons
-          const BAppBar(
-            showBackArrow: true, 
-            actions: [
-              BCircularIcon(icon: Iconsax.heart5, color: Colors.red)
+
+              // Image Slider.
+
+              Positioned(
+                right: 0,
+                bottom: 30,
+                left: BSizes.defaultSpace,
+                child: SizedBox(
+                  height: 80,
+                  child: ListView.separated(
+                    itemCount: images.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    separatorBuilder: (_, __) => const SizedBox(
+                      width: BSizes.spaceBtwItems,
+                    ),
+                    itemBuilder: (_, index) => BRoundedImage(
+                      width: 80,
+                      backgroundColor: dark ? BColors.dark : BColors.white,
+                      imageUrl: images[index], 
+                      border: Border.all(color: BColors.primary),
+                      padding: const EdgeInsets.all(BSizes.sm),
+                    ),
+                  ),
+                ),
+              ),
+
+              // AppBar Icons
+              const BAppBar(
+                showBackArrow: true,
+                actions: [
+                  BCircularIcon(icon: Iconsax.heart5, color: Colors.red)
+                ],
+              )
             ],
-          )
-          
-        ],
-      )
-      ),
+          )),
     );
   }
 }
