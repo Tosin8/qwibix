2a133a7183786de1f205e3acbce7124003e560f6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-02 11:25:54 +0100

    Update brand_products.dart

--- lib/features/shop/screens/extensions/brands/brand_products.dart
+++ lib/features/shop/screens/extensions/brands/brand_products.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qwibix/common/widgets/appbar/appbar.dart';
 import 'package:qwibix/common/widgets/products/product_cards/brand_card.dart';
-import 'package:qwibix/common/widgets/products/sortable.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
 
 class BrandProducts extends StatelessWidget {
