6cfbba97901aca914d4fe1368828fd4953da143b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-03 05:50:31 +0100

    Update all_brand.dart

--- lib/features/shop/screens/extensions/brands/all_brand.dart
+++ lib/features/shop/screens/extensions/brands/all_brand.dart
@@ -4,6 +4,7 @@ import 'package:qwibix/common/widgets/appbar/appbar.dart';
 import 'package:qwibix/common/widgets/grid_layout/grid_layout.dart';
 import 'package:qwibix/common/widgets/products/product_cards/brand_card.dart';
 import 'package:qwibix/common/widgets/texts/sectionHeading.dart';
+import 'package:qwibix/features/shop/controllers/brand_controller.dart';
 import 'package:qwibix/features/shop/controllers/category_controller.dart';
 import 'package:qwibix/features/shop/models/brand_model.dart';
 import 'package:qwibix/utils/constants/sizes.dart';
@@ -15,29 +16,37 @@ class AllBrandsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold( 
-      appBar: const BAppBar(title: Text('All Brands'), 
-      showBackArrow: true,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(padding: const EdgeInsets.all(BSizes.defaultSpace), 
-        child: Column(
-          children: [
-
-            // Heading
-            const BSectionHeading(title: 'Brands', showActionButton: false,),
-            const SizedBox(height: BSizes.spaceBtwItems), 
-
-            // Bramds
-            GridLayout(itemCount: 6, mainAxisExtent: 80,
-            
-             itemBuilder: (_, index) => BrandCard(showBorder: true, onTap: () => Get.to(() => const BrandProducts()), brand: BrandModel.empty(),),)
-          ],
+    final brandController = Get.put(BrandController());
+    return Scaffold(
+        appBar: const BAppBar(
+          title: Text('All Brands'),
+          showBackArrow: true,
         ),
-        ), 
-        
-      )
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(BSizes.defaultSpace),
+            child: Column(
+              children: [
+                // Heading
+                const BSectionHeading(
+                  title: 'Brands',
+                  showActionButton: false,
+                ),
+                const SizedBox(height: BSizes.spaceBtwItems),
+
+                // Bramds
+                Obx(() => GridLayout(
+                      itemCount: brandController.allBrands.length,
+                      mainAxisExtent: 80,
+                      itemBuilder: (_, index) => BrandCard(
+                        showBorder: true,
+                        onTap: () => Get.to(() => const BrandProducts()),
+                        brand: BrandModel.empty(),
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
