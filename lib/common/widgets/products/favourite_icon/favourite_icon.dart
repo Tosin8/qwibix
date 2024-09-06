4b163eba65b34df094ea7b44dccd1bda170ce1c5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-06 23:26:19 +0100

    Update favourite_icon.dart

---
+++ lib/common/widgets/products/favourite_icon/favourite_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:qwibix/utils/constants/circular_icon.dart';
+
+class FavouriteIcon extends StatelessWidget {
+  const FavouriteIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BCircularIcon( icon: Iconsax.heart5, onPressed: (){},color: Colors.red, );
+  }
+}
