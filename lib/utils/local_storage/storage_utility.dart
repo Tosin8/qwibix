d1c5215cf564435fff5898482f1780b7947ac5fd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-08 18:15:08 +0100

    Update storage_utility.dart

--- lib/utils/local_storage/storage_utility.dart
+++ lib/utils/local_storage/storage_utility.dart
@@ -1,41 +1,45 @@
 import 'package:get_storage/get_storage.dart';
 
 class BLocalStorage {
-  static final BLocalStorage _instance = BLocalStorage._internal();
+  
+  static BLocalStorage? _instance; 
+  GetStorage? _storage; 
+  BLocalStorage._internal(); 
 
-  factory BLocalStorage() {
-    return _instance;
-  }
+  factory BLocalStorage.instance() {
+  _instance ??= BLocalStorage._internal(); 
+  return _instance!;
+    }
 
-  BLocalStorage._internal();
+  
 
-  final _storage = GetStorage();
+  
 
 static Future<void> init(String bucketName) async {
   await GetStorage.init(bucketName);
   _instance = BLocalStorage._internal(); 
-  _instance._storage = GetStorage(bucketName); 
+  _instance!._storage = GetStorage(bucketName); 
 }
 
 
   // Generic method to save data
   Future<void> saveData<T>(String key, T value) async {
-    await _storage.write(key, value);
+    await _storage?.write(key, value);
   }
 
   // Generic method to read data
   T? readData<T>(String key) {
-    return _storage.read<T>(key);
+    return _storage?.read<T>(key);
   }
 
   // Generic method to remove data
   Future<void> removeData(String key) async {
-    await _storage.remove(key);
+    await _storage?.remove(key);
   }
 
   // Clear all data in storage
   Future<void> clearAll() async {
-    await _storage.erase();
+    await _storage?.erase();
   }
 }
 
