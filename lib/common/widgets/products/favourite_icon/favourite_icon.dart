e6546adeaa306070cd73f5c40edb065a5288ba92
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-08 18:46:59 +0100

    Update favourite_icon.dart

--- lib/common/widgets/products/favourite_icon/favourite_icon.dart
+++ lib/common/widgets/products/favourite_icon/favourite_icon.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:qwibix/utils/constants/circular_icon.dart';
 
